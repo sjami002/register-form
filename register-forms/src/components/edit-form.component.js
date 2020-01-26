6e04603a71421a828f745bab61bb5b9f2fa4e6c4
github username <sjami002@gmail.com>
2020-01-26 11:51:51 -0500

    form list/edit/del

--- register-forms/src/components/edit-form.component.js
+++ register-forms/src/components/edit-form.component.js
@@ -1,9 +1,163 @@
 import React, { Component } from "react";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-class EditForm extends Component {
+export default class EditForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangePlan = this.onChangePlan.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      description: "",
+      plan: 0,
+      date: new Date(),
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/forms/" + this.props.match.params.id)
+      .then(response => {
+        this.setState({
+          username: response.data.username,
+          description: response.data.description,
+          plan: response.data.plan,
+          date: new Date(response.data.date)
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    axios
+      .get("http://localhost:5000/users/")
+      .then(response => {
+        if (response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username)
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangePlan(e) {
+    this.setState({
+      plan: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const form = {
+      username: this.state.username,
+      description: this.state.description,
+      plan: this.state.plan,
+      date: this.state.date
+    };
+
+    console.log(form);
+
+    axios
+      .post(
+        "http://localhost:5000/forms/update/" + this.props.match.params.id,
+        form
+      )
+      .then(res => console.log(res.data));
+
+    window.location = "/";
+  }
+
   render() {
-    return <div>This is the Edit Forms Component</div>;
+    return (
+      <div>
+        <h3>Edit Form Log</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              ref="userInput"
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map(function(user) {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Plan (Level 1-4): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.plan}
+              onChange={this.onChangePlan}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Edit Form Log"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
-
-export default EditForm;
--- register-forms/src/components/form-list.component.js
+++ register-forms/src/components/form-list.component.js
@@ -1,9 +1,86 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-class FormsList extends Component {
+const Form = props => (
+  <tr>
+    <td>{props.form.username}</td>
+    <td>{props.form.description}</td>
+    <td>{props.form.plan}</td>
+    <td>{props.form.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.form._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteForm(props.form._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
+
+export default class FormsList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteForm = this.deleteForm.bind(this);
+
+    this.state = { forms: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/forms/")
+      .then(response => {
+        this.setState({ forms: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  deleteForm(id) {
+    axios.delete("http://localhost:5000/forms/" + id).then(response => {
+      console.log(response.data);
+    });
+
+    this.setState({
+      forms: this.state.forms.filter(el => el._id !== id)
+    });
+  }
+
+  formList() {
+    return this.state.forms.map(currentform => {
+      return (
+        <Form
+          form={currentform}
+          deleteForm={this.deleteForm}
+          key={currentform._id}
+        />
+      );
+    });
+  }
+
   render() {
-    return <div>This is the Forms List Component</div>;
+    return (
+      <div>
+        <h3>Logged Forms</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Plan</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.formList()}</tbody>
+        </table>
+      </div>
+    );
   }
 }
-
-export default FormsList;
