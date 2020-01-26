ae2c5d82d1b978b4c9363962c7b3eb1a2c4933bb
github username <sjami002@gmail.com>
2020-01-26 14:22:48 -0500

    staging deployment

--- client/backend/server.js
+++ client/backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Step 2
+// Step 2 db
 const uri = process.env.ATLAS_URI;
 mongoose.connect(
   uri || process.env.MONGODB_URI,
