2325fc02251cd7e7e46f1a2f617fd56b35479d8f
github username <sjami002@gmail.com>
2020-01-26 14:10:17 -0500

    client

--- client/backend/server.js
+++ client/backend/server.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 require("dotenv").config();
 
 const app = express();
+// Step 1
 const port = process.env.PORT || 5000;
 
 //Middleware
 app.use(cors());
 app.use(express.json());
 
+// Step 2
 const uri = process.env.ATLAS_URI;
 mongoose.connect(
-  uri,
+  uri || process.env.MONGODB_URI,
   { useNewUrlParser: true, useCreateIndex: true }
 );
 const connection = mongoose.connection;
@@ -26,6 +29,15 @@ const usersRouter = require("./routes/users");
 
 app.use("/forms", formsRouter);
 app.use("/users", usersRouter);
+
+// Step 3
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "client", "build", "index.html")); //relative path
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
